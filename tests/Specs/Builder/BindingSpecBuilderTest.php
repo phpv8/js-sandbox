ce2b1de2439a0ce0f4ac4a7cbae6b371a43f6f40
Bogdan Padalko <pinepain@gmail.com>
2017-10-14 20:40:18 +0300

    Add mised docblock and reforamt tests

--- tests/Specs/Builder/BindingSpecBuilderTest.php
+++ tests/Specs/Builder/BindingSpecBuilderTest.php
@@ -1,14 +1,26 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the pinepain/js-sandbox PHP library.
+ *
+ * Copyright (c) 2016-2017 Bogdan Padalko <pinepain@gmail.com>
+ *
+ * Licensed under the MIT license: http://opensource.org/licenses/MIT
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source or visit
+ * http://opensource.org/licenses/MIT
+ */
+
 
 namespace Pinepain\JsSandbox\Tests\Specs\Builder;
 
 
+use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject;
 use Pinepain\JsSandbox\Specs\BindingSpecInterface;
 use Pinepain\JsSandbox\Specs\Builder\BindingSpecBuilder;
 use Pinepain\JsSandbox\Specs\Builder\BindingSpecBuilderInterface;
-use PHPUnit\Framework\TestCase;
 use Pinepain\JsSandbox\Specs\Builder\Exceptions\FunctionSpecBuilderException;
 use Pinepain\JsSandbox\Specs\Builder\Exceptions\PropertySpecBuilderException;
 use Pinepain\JsSandbox\Specs\Builder\FunctionSpecBuilderInterface;
